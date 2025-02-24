05d332772cc3b67a354df02e54120ef7057dc4a4
Daniel Molenda <daniel.molenda@shoper.pl>
2025-02-24 13:08:24 +0100

    🔧 Update script

--- .github/workflows/scripts/package.sh
+++ .github/workflows/scripts/package.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-package_version=$(grep -e 'version:' charts/phpipam/Chart.yaml | awk '{ print $NF }')
-package_name="phpipam-${package_version}"
+package_version="$(grep -e "version:" charts/phpipam/Chart.yaml | awk '{ print $NF }' | sed 's/\r//g')"
+package_name="phpipam-${package_version}.tgz"
 
-if [ ! -f "packages/${package_name}.tgz" ]; then
-    helm package --destination packages charts/*
-    helm repo index . --url https://dreamcommerce.github.io/phpipam-helm/
-else
+if [ -f "packages/$package_name" ]
+then
     echo "Package already exists: ${package_name}"
     exit 0
+else
+    helm package --destination packages charts/*
+    helm repo index . --url https://dreamcommerce.github.io/phpipam-helm/
 fi
