865f9d55737299412a38c07c5b96e9cfd499d310
Daniel Molenda <daniel.molenda@shoper.pl>
2025-02-24 13:15:37 +0100

    🔧 Update script

--- .github/workflows/scripts/package.sh
+++ .github/workflows/scripts/package.sh
@@ -6,8 +6,10 @@ package_name="phpipam-${package_version}.tgz"
 if [ -f "packages/$package_name" ]
 then
     echo "Package already exists"
+    echo "package_exist=true" >> "$GITHUB_OUTPUT"
     exit 0
 else
     helm package --destination packages charts/*
     helm repo index . --url https://dreamcommerce.github.io/phpipam-helm/
+    echo "package_exist=false" >> "$GITHUB_OUTPUT"
 fi
