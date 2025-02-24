b42489d43e3fb27ed91cc08b5e6ccca7099a2396
Daniel Molenda <daniel.molenda@shoper.pl>
2025-02-24 12:50:47 +0100

    🔧 Update path

--- .github/workflows/scripts/package.sh
+++ .github/workflows/scripts/package.sh
@@ -3,7 +3,7 @@
 package_version=$(grep -e 'version:' charts/phpipam/Chart.yaml | awk '{ print $NF }')
 package_name="phpipam-${package_version}.tgz"
 
-if [ ! -f "${package_name}" ]; then
+if [ ! -f "packages/${package_name}" ]; then
     helm package --destination packages charts/*
     helm repo index . --url https://dreamcommerce.github.io/phpipam-helm/
 else
